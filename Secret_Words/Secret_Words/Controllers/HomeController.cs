25e3961c180e2bdcc529bf2d5381514d24441d5e
jjenks97 <jonathan.jenkins97@gmail.com>
2017-05-10 01:44:18 -0400

    first commit

---
+++ Secret_Words/Secret_Words/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Secret_Words.Data;
+using Secret_Words.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Secret_Words.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> user;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userman)
+        {
+            db = context;
+            user = userman;
+        }
+        public IActionResult Index()
+        {
+            var word = db.Secret_Words
+                .OrderByDescending(am => am.TimeStamp).First();
+
+            return View(word);
+        }
+
+        public IActionResult AddWord(string Secret_Words)
+        {
+            Secret_WordsModel newWord = new Secret_WordsModel();
+            newWord.TimeStamp = DateTime.Now;
+            newWord.Username = User.Identity.Name;
+            newWord.Word = Secret_Words;
+            db.Secret_Words.Add(newWord);
+            db.SaveChanges();
+
+            return Redirect(@"/Home/Index");
+        }
+        public IActionResult Words()
+        {
+            var allWords = db.Secret_Words.OrderByDescending(am => am.TimeStamp);
+            return View(allWords);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Secret_Words/Secret_Words/Models/WordModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Secret_Words.Models
+{
+    public class WordModel
+    {
+    }
+}
